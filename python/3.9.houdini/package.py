5c07e5ebf7ba47f805d0a5e8c538c0259889586e
Jonathan Gerber <jlgerber@gmail.com>
2022-08-13 07:41:54 -0700

    more houdini

---
+++ python/3.9.houdini/package.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+name = 'python'
+
+version = '3.9.houdini'
+
+variants = [
+            #['platform-linux', 'arch-x86_64', 'os-Pop-21.10'],
+            ['platform-linux', 'arch-x86_64', 'os-Pop-22.04'],
+        ]
+
+build_command = "python {root}/build_cmd.py {install}"
+
+uuid = "external.python"
+
+def commands():
+    #env.PATH.append('{this.root}/bin')
+    pass
+
+def post_commands():
+    # these are the builtin modules for this python executable. If we don't
+    # include these, some python behavior can be incorrect.
+    if not building:    
+        env.PYTHONHOME="${HFS}/python"
+        env.PATH.prepend("${HFS}/python/bin") 
