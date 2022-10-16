13d2b2c7b8b91d5939f3bcc1eb3d839f4151f952
Jonathan Gerber <jlgerber@gmail.com>
2022-10-16 07:14:12 -0700

    add houdini 19.5.403

---
+++ houdini/19.5.403/package.py
@@ -0,0 +1,96 @@
+name = "houdini"
+
+variants = [
+    ["platform-linux", "arch-x86_64", "python-3.9.houdini"]
+]
+
+build_command = "python {root}/build_cmd.py {install}"
+tools = [
+    'houdini',
+    'houdinifx',
+    'mview',
+    'mplay',
+    'abcconvert',
+    'abcecho',
+    'abcinfo',
+    'acombine',
+    'acombine-bin',
+    'aconvert',
+    'aconvert-bin',
+    'ainfo',
+    'ainfo-bin',
+    'aplay',
+    'aplay-bin',
+    'husk',
+    'hbatch',
+    'hcommand',
+    'hcompile',
+    'hserver',
+    'hview',
+    'hython',
+    'hotl',
+    'mantra',
+    'mplay',
+    'karma',
+    'mplay-bin',
+    'usdcat',
+    'usdchecker',
+    'usddiff',
+    'usddumpcrate',
+    'usdedit',
+    'usdrecord',
+    'usdresolve',
+    'usdstitch',
+    'usdstichclips',
+    'usdtree',
+    'usdview',
+    'usdzip'
+]
+
+has_plugins = True
+
+uuid = "repository.houdini"
+
+@early()
+def version():
+    import os
+    return os.path.basename(os.path.abspath(os.getcwd()))
+
+@early()
+def version_p():
+    import os
+    return os.path.basename(os.path.abspath(os.getcwd())).split(".")
+
+def pre_commands():
+    if building:
+        print("building")
+        #env.PYTHONHOME="/home/jgerber/packages/houdini/19.5.333/platform-linux/arch-x86_64/python-3.9.houdini/app/python"
+def pre_build_commands():
+        print("prebuild")
+        #env.PYTHONHOME="/home/jgerber/packages/houdini/19.5.333/platform-linux/arch-x86_64/python-3.9.houdini/app/python"
+
+def commands():
+    env.HFS = "{root}/app"
+    env.H="${HFS}"
+    env.HB = "${H}/bin"
+    env.HDSO = "${H}/dsolib"
+    env.HH = "${H}/houdini"
+    env.HHC = "${HH}/config"
+    env.HHP = "${HH}/python3.7libs"
+    env.HT = "${H}/toolkit"
+    env.HSB = "${HH}/sbin"
+    env.TEMP="/tmp"
+    env.LD_PRELOAD = "/lib/x86_64-linux-gnu/libc_malloc_debug.so.0"    
+    env.LD_LIBRARY_PATH.prepend("${HDSO}")
+    env.PATH.prepend("${HSB}")
+    env.PATH.prepend("${HB}")
+    env.HOUDINI_MAJOR_RELEASE = this.version_p[0]
+    env.HOUDINI_MINOR_RELEASE = this.version_p[1]
+    env.HOUDINI_BUILD_VERSION = this.version_p[2]
+    env.HOUDINI_VERSION = this.version
+    env.HOUDINI_BUILD_KERNEL = "3.10.0-1160.42.2.el7.x86_64"
+    env.HOUDINI_BUILD_PLATFORM ="Red Hat Enterprise Linux Workstation release 7.9 (Maipo)"
+    env.HOUDINI_BUILD_COMPILER="9.3.1"
+    env.HOUDINI_BUILD_LIBC="glibc 2.17"
+    env.HOUDINI_PYTHON_PANEL_PATH="&"
+    env.HOUDINI_OTLSCAN_PATH="&"
