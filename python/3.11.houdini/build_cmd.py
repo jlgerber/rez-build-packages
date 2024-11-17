f363ff71aa72eb73dea76f0aaafb14f94e81c57d
Jonathan Gerber <jlgerber@gmail.com>
2024-11-17 09:13:06 -0800

    update packages

---
+++ python/3.11.houdini/build_cmd.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import os
+import sys
+
+
+installing = False
+if len(sys.argv) > 1 and sys.argv[1] == "install":
+    installing = True
+
+print("installing python")
+
+
+
+
+
